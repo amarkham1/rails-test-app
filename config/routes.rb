a9ec15891e51633ab2d35e8c4248ab5ae9db6645
amarkham1 <andrewjmarkham1@gmail.com>
2020-05-23 12:43:46 -0400

    added user signup page

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
   resources :articles
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
 end
