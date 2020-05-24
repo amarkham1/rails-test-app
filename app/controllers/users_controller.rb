264ea550c160c1150a39843a7f3721107f8d4366
amarkham1 <andrewjmarkham1@gmail.com>
2020-05-24 12:11:12 -0400

    added user profiles and paginated articles and users list

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,12 @@
 class UsersController < ApplicationController
-def show
-  @user = User.find(params[:id])
-  @articles = @user.articles
-end
+  def show
+    @user = User.find(params[:id])
+    @articles = @user.articles.paginate(page: params[:page], per_page: 5)
+  end
+
+  def index
+    @users = User.paginate(page: params[:page], per_page: 5)
+  end
 
   def new
     @user = User.new
@@ -26,7 +30,7 @@ end
     @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "Your account information was updated successfully."
-      redirect_to articles_path
+      redirect_to @user
     else
       render 'edit'
     end
